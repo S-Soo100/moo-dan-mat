7fab30a959a32644d01050f7c9151bda1c908bc0
백승수 <bss.rol20@gmail.com>
2023-12-11 16:08:48 +0900

    client 추가, powder 받아오기 추가

--- src/service/powder.ts
+++ src/service/powder.ts
@@ -1,11 +1,8 @@
 import { client } from "./sanity";
 
 export async function getPowders() {
-  return (
-    client
-      .fetch(`*[_type == "powder"]`)
-      // 초기 객체 타입 명시, 받으면 어떤 형태로 받아오는지 명시 필요
-      .catch()
-      .then((powder) => ({...powder}));
-  );
+  return client
+    .fetch(`*[_type == "powder"]`)
+    .catch()
+    .then((powder) => ({ ...powder }));
 }
